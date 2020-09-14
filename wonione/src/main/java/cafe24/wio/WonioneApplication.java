6e4abc924ed1db7ffeb121272d03fb4bea6c90ad
xsxs112 <xsxs112@naver.com>
2020-09-14 10:51:07 +0900

    [WonIOne] Start ProjectWoniOne file: * detail: First Commit and Push

---
+++ wonione/src/main/java/cafe24/wio/WonioneApplication.java
@@ -0,0 +1,13 @@
+package cafe24.wio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WonioneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WonioneApplication.class, args);
+	}
+
+}
---
+++ wonione/src/test/java/cafe24/wio/WonioneApplicationTests.java
@@ -0,0 +1,13 @@
+package cafe24.wio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WonioneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
