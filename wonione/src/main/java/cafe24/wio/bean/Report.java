23fd6e3c06d9e45b9c0fcb7e8caab4b9aa38a3f2
dolcevitaa <chocolateyaa@naver.com>
2020-09-15 12:42:43 +0900

    [WonIOne] 이슬이개인프로젝트수정 file:   leftmenu.html 		cafe24.wio.bean-Report.java 		cafe24.wio.controller- 		WorkReportController.java 		cafe24.wio.mapper-ReportMapper.java 		cafe24.wio.service-ReportService.java 		mapper-ReportMapper.xml 		humanresource-workreport.html
    detail:
    		업무계획보고서
    		패키지내 클래스
    		controller, service, mapper,
    		humanresource폴더내workReport.html수정

--- wonione/src/main/java/cafe24/wio/bean/Report.java
+++ wonione/src/main/java/cafe24/wio/bean/Report.java
@@ -1,5 +1,84 @@
 package cafe24.wio.bean;
 
 public class Report {
-
+	
+	private String lecOpenCode;
+	private String lecOpenScheCode;
+	private String lecTuition;
+	private String mrId;
+	private String lecTarget;
+	private String lecPlan;
+	private String lecEtc;
+	private String openLecRegDate;
+	
+	@Override
+	public String toString() {
+		StringBuilder builder = new StringBuilder();
+		builder.append("Report [lecOpenCode=");
+		builder.append(lecOpenCode);
+		builder.append(", lecOpenScheCode=");
+		builder.append(lecOpenScheCode);
+		builder.append(", lecTuition=");
+		builder.append(lecTuition);
+		builder.append(", mrId=");
+		builder.append(mrId);
+		builder.append(", lecTarget=");
+		builder.append(lecTarget);
+		builder.append(", lecPlan=");
+		builder.append(lecPlan);
+		builder.append(", lecEtc=");
+		builder.append(lecEtc);
+		builder.append(", openLecRegDate=");
+		builder.append(openLecRegDate);
+		builder.append("]");
+		return builder.toString();
+	}
+	public String getLecOpenCode() {
+		return lecOpenCode;
+	}
+	public void setLecOpenCode(String lecOpenCode) {
+		this.lecOpenCode = lecOpenCode;
+	}
+	public String getLecOpenScheCode() {
+		return lecOpenScheCode;
+	}
+	public void setLecOpenScheCode(String lecOpenScheCode) {
+		this.lecOpenScheCode = lecOpenScheCode;
+	}
+	public String getLecTuition() {
+		return lecTuition;
+	}
+	public void setLecTuition(String lecTuition) {
+		this.lecTuition = lecTuition;
+	}
+	public String getMrId() {
+		return mrId;
+	}
+	public void setMrId(String mrId) {
+		this.mrId = mrId;
+	}
+	public String getLecTarget() {
+		return lecTarget;
+	}
+	public void setLecTarget(String lecTarget) {
+		this.lecTarget = lecTarget;
+	}
+	public String getLecPlan() {
+		return lecPlan;
+	}
+	public void setLecPlan(String lecPlan) {
+		this.lecPlan = lecPlan;
+	}
+	public String getLecEtc() {
+		return lecEtc;
+	}
+	public void setLecEtc(String lecEtc) {
+		this.lecEtc = lecEtc;
+	}
+	public String getOpenLecRegDate() {
+		return openLecRegDate;
+	}
+	public void setOpenLecRegDate(String openLecRegDate) {
+		this.openLecRegDate = openLecRegDate;
+	}
 }
