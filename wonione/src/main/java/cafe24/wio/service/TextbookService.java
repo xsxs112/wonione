910d4d71b2a923a5fbe43313d90d78ff5233a552
xsxs112 <xsxs112@naver.com>
2020-09-16 17:24:50 +0900

    [WonIOne] 교재입고등록 author: 이원석 file: 	textbookWahoRegister.html 	TextbookMapper.xml 	TextbookService.java 	TextbookMapper.java 	TextbookController.java detail: 	1)	교재 입고 등록 MVC 	2)	겟매핑에서 교재기본정보와 	목록을 조회하는 서비스를 		호출하여 모델에 담아 뷰로 포워드시킴 	3)	포스트 매핑에서 교재입고서비스 호출하고 		교재관리메인페이지로 	리다이렉트 처리 	4)	교재입고등록 쿼리문 PK VALUE값에 		코드자동증가 서브쿼리 추가 	5)	재고수량value값에 서브쿼리 이용하여 최근날짜의 		재고수량을 조회한 후 입력받은 입고수량을 더해줌 	6)	작성자아이디는 추후에 로그인한 세션아이디값을 넣어줄 예정 	7)	placeholder에 타임리프로 현재까지 		재고수량값을 출력해줄	예정

--- wonione/src/main/java/cafe24/wio/service/TextbookService.java
+++ wonione/src/main/java/cafe24/wio/service/TextbookService.java
@@ -15,16 +15,28 @@ public class TextbookService {
 	@Autowired
 	private TextbookMapper textbookMapper;
 	
+	//교재 입고 테이블 조회
+	public List<WhTextbook> getWhTextbookList(){
+		
+		List<WhTextbook> whTextbookList = textbookMapper.getWhTextbookList();
+		
+		return whTextbookList;
+	}
+	
+	//교재 기초정보 조회
 	public List<TextbookBasicInfo> getTextbookInfoList(TextbookBasicInfo txbBasicInfo){
 		List<TextbookBasicInfo> textbookinfolist = textbookMapper.getTextbookInfoList(txbBasicInfo);
 		
 		return textbookinfolist;
 	}
 	
+	//보유 교재리스트 조회
 	public List<TextbookBasicInfo> getTextbookOwnList(WhTextbook whTextbook){
 		List<TextbookBasicInfo> textbookOwnList = textbookMapper.getTextbookOwnList(whTextbook);
 		return textbookOwnList;
 	}
+	
+	//교재 입고등록
 	public int addWhTextbook(WhTextbook whTextbook) {
 		
 		int addWhTxbResult = textbookMapper.addWhTextbook(whTextbook);
