f70ca6cdc101001173c9d8537dbb937924261b14
algus1951 <algus1951@naver.com>
2020-09-16 11:28:20 +0900

    file: hollydayApproval.html 	Approval.java 	WorkCotroller.java 	leftmenu.html
    detail:
    
    추가 내용 hollydayApproval.html
    	Approval.java 생성
    	WorkCotroller.java holidayApproval 메소드 생성
    	leftmenu.html 휴가신청 li 만들고 경로 변경

--- wonione/src/main/java/cafe24/wio/controller/WorkController.java
+++ wonione/src/main/java/cafe24/wio/controller/WorkController.java
@@ -1,11 +1,9 @@
 package cafe24.wio.controller;
 
-import javax.servlet.http.HttpSession;
-
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
- 
+
+import cafe24.wio.bean.ApprovalRequest;
 import cafe24.wio.bean.WorkAttendance;
 
 //황미현 - 출퇴근 컨트롤러
@@ -18,6 +16,13 @@ public class WorkController {
 		
 		
 		return "workmanagment/workAttendance";
+	}
+	
+	@GetMapping("/holidayApproval")
+	public String holidayApproval(ApprovalRequest approvalRequest) {
+		
+		
+		return "workmanagment/holidayApproval";
 	}
 	
 
