a51e6e1b414ba6eb8bf8a4632a51a398a8878fbd
algus1951 <algus1951@naver.com>
2020-09-16 16:14:40 +0900

    author: 황미현 file: holidayLogin.html detail: holidayLogin.html 생성

--- wonione/src/main/java/cafe24/wio/service/ApprRequestService.java
+++ wonione/src/main/java/cafe24/wio/service/ApprRequestService.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import cafe24.wio.bean.ApprovalRequest;
+import cafe24.wio.bean.Member;
 import cafe24.wio.mapper.ApprMapper;
 
 @Service
@@ -21,6 +22,15 @@ public class ApprRequestService {
 		
 		
 		return holliday;
+	}
+	
+	public int addholidayApproval(Member member,ApprovalRequest approvalRequest) {
+
+
+		int result = apprMapper.addholidayApproval(member, approvalRequest);
+		
+		
+		return result;
 	}
 	
 
