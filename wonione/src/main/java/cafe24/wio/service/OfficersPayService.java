4b0c77b024745b1878cfdf527b653b55a7ce0a1c
HYJ <ghkssla@naver.com>
2020-09-18 01:00:52 +0900

    [wonione] 추가
    file:
    OfficersPay.java < 필드추가
    officer 관련 파일 모두 수정
    detail:

--- wonione/src/main/java/cafe24/wio/service/OfficersPayService.java
+++ wonione/src/main/java/cafe24/wio/service/OfficersPayService.java
@@ -14,33 +14,40 @@ public class OfficersPayService {
 	@Autowired
 	private OfficersPayMapper officersPayMapper;
 	
-	public List<OfficersPay> getOfficersPayList() {
-		
-		//직원들의 정보가 담긴 list객체
-		List<OfficersPay> officersPayList = officersPayMapper.getOfficersPayList();
-	
-		return officersPayList;
-	}
 
-	public List<OfficersPay> getOfficersPay(String mrId) {
-		//직원의 id 정보가 담긴 list객체
-		List<OfficersPay> officersPay = officersPayMapper.getOfficersPay(mrId);
-		
-		return officersPay;
-	}
-
-	public List<OfficersPay> getOfficersList() {
-		//직원의 급여 정보가 담긴 list객체
+	//직원 목록조회
+	public List<OfficersPay> getOfficersList() {	
 		List<OfficersPay> OfficersList = officersPayMapper.getOfficersList();
 		
 		return OfficersList;
 	}
-
+	
+	//자원테이블 가져오기
+	public List<OfficersPay> standardSheet() {
+		List<OfficersPay> standardSheetHour = officersPayMapper.standardSheet();
+		
+		return standardSheetHour;
+	}	
+	
+	//직원 한명 정보 조회
 	public OfficersPay callOfficersInfo(String mrId) {
-		//직원의 정보가 담긴 list객체
 		OfficersPay OfficersInfo = officersPayMapper.callOfficersInfo(mrId);
 		
 		return OfficersInfo;
+	}
+	
+	//직원 급여목록 조회
+	public List<OfficersPay> getOfficersPayList() {		
+		
+		List<OfficersPay> officersPayList = officersPayMapper.getOfficersPayList();	
+		return officersPayList;
+	}
+	
+	//직원 급여명세 조회
+	public List<OfficersPay> getOfficersPay(String mrId) {
+		
+		List<OfficersPay> officersPay = officersPayMapper.getOfficersPay(mrId);		
+		return officersPay;
 	}
 
 }
