ad3ae64fc1b9c16eac0237f7b8c7ca7983cf10b8
HYJ <ghkssla@naver.com>
2020-09-19 21:31:49 +0900

    [wonione] 강사 급여관려 html 작성 file: payList.html 		StaffPayMapper.xml 		staffPay.html 		staffPayDocumet.html detail: 화면 먼저 만듬

---
+++ wonione/src/main/java/cafe24/wio/mapper/StaffPayMapper.java
@@ -0,0 +1,27 @@
+package cafe24.wio.mapper;
+
+import java.util.List;
+
+import org.apache.ibatis.annotations.Mapper;
+
+import cafe24.wio.bean.OfficersPay;
+
+@Mapper
+public interface StaffPayMapper {
+	
+	//직원 급여 목록 조회
+	public List<OfficersPay> getOfficersPayList();
+
+	//직원급여명세서 조회
+	public List<OfficersPay> getOfficersPay(String mrId);
+
+	//직원 목록 조회
+	public List<OfficersPay> getOfficersList();
+	
+	//자원테이블 호출
+	public List<OfficersPay> standardSheet();
+
+	//직원 정보 조회
+	public OfficersPay callOfficersInfo(String mrId);
+
+}
