cd9600632a71dbd64fdb5f07fc80486fc97d1297
user <user@DESKTOP-HAI702R>
2020-10-13 17:24:36 +0900

    [WonIOne] author:	정진수 file: 	member, Course detail: 1. 과정 선택 화면 완료 		2. addMember 에서 데이터 넘겨 받는 작업 진행해야함. 		3. 유효성 검사 진행중. 미완성.

--- wonione/src/main/java/cafe24/wio/bean/Course.java
+++ wonione/src/main/java/cafe24/wio/bean/Course.java
@@ -4,10 +4,16 @@ public class Course {
 	
 	// 수업 과정 Bean
 	
-	private String courseNum;	// 과정 테이블 PK
-	private String lecOsCode;	// 강의 개설 예정 코드
-	private String appSid;		// 신청 학생 아이디
-	private String appGroupSid; // 그룹화 짓는 코드
+	private String 	courseNum;		// 과정 테이블 PK
+	private String 	lecOsCode;		// 강의 개설 예정 코드
+	private String 	lecName;		// 강의명
+	private String 	lecDay;			// 진행 요일
+	private String 	lecStartTime;	// 수업 시작 시간
+	private String 	lecEndTime;		// 수업 종료 시간
+	private int 	lecTuition;		// 단가
+	private int 	claCapacity;	// 수강 정원
+	private String 	appSid;			// 신청 학생 아이디
+	private String 	appGroupSid; 	// 그룹화 코드
 	
 	
 	public String getCourseNum() {
@@ -22,6 +28,42 @@ public class Course {
 	public void setLecOsCode(String lecOsCode) {
 		this.lecOsCode = lecOsCode;
 	}
+	public String getLecName() {
+		return lecName;
+	}
+	public void setLecName(String lecName) {
+		this.lecName = lecName;
+	}
+	public String getLecDay() {
+		return lecDay;
+	}
+	public void setLecDay(String lecDay) {
+		this.lecDay = lecDay;
+	}
+	public String getLecStartTime() {
+		return lecStartTime;
+	}
+	public void setLecStartTime(String lecStartTime) {
+		this.lecStartTime = lecStartTime;
+	}
+	public String getLecEndTime() {
+		return lecEndTime;
+	}
+	public void setLecEndTime(String lecEndTime) {
+		this.lecEndTime = lecEndTime;
+	}
+	public int getLecTuition() {
+		return lecTuition;
+	}
+	public void setLecTuition(int lecTuition) {
+		this.lecTuition = lecTuition;
+	}
+	public int getClaCapacity() {
+		return claCapacity;
+	}
+	public void setClaCapacity(int claCapacity) {
+		this.claCapacity = claCapacity;
+	}
 	public String getAppSid() {
 		return appSid;
 	}
@@ -35,7 +77,6 @@ public class Course {
 		this.appGroupSid = appGroupSid;
 	}
 	
-	
 	@Override
 	public String toString() {
 		StringBuilder builder = new StringBuilder();
@@ -43,6 +84,18 @@ public class Course {
 		builder.append(courseNum);
 		builder.append(", lecOsCode=");
 		builder.append(lecOsCode);
+		builder.append(", lecName=");
+		builder.append(lecName);
+		builder.append(", lecDay=");
+		builder.append(lecDay);
+		builder.append(", lecStartTime=");
+		builder.append(lecStartTime);
+		builder.append(", lecEndTime=");
+		builder.append(lecEndTime);
+		builder.append(", lecTuition=");
+		builder.append(lecTuition);
+		builder.append(", claCapacity=");
+		builder.append(claCapacity);
 		builder.append(", appSid=");
 		builder.append(appSid);
 		builder.append(", appGroupSid=");
