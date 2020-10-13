2cee6e0f95ca8ccd10222f48899d694477e99fdf
Won-seok LEE <xsxs112@naver.com>
2020-10-13 17:28:00 +0900

    [WonIOne] 강의리스트, 강의예정리스트 author: 이원석 file: 	lecList.html 	lecOsList.html 	LectureController.java 	LectureMapper.java 	LectureMapper.xml 	LectureService.java detail: 	1.강의리스트 	1)강의상태변경 Modal 	-강의 상태변경버튼 클릭시 Modal내의 값 들 변경 	-스크립트내에서 타임리프 사용 시 CDATA를 이용해야함 	-이후 each문으로 배열을 풀어주었음 	2)강의상태변경 Modal - ajax 	-Modal 내의 확인버튼 클릭시 클릭이벤트 	-해당 강의개설코드와 선택된 강의상태 파라미터를 보내줌 	-요청 url컨트롤러에서 update관련된 mvc로직실행 	-업데이트 성공시 리스트페이지로 리다이렉트처리 	2.강의예정리스트 	1.개강버튼클릭시 	-해당 예정코드로 이미 개설되어있는지 확인하는 ajax 스크립트 	-이미 개강되었으면 1을, 아니면 0을 리턴시켜줌 	-0일때 선택한 예정코드로 강의를 개설시키도록 ajax처리할 예정

--- wonione/src/main/java/cafe24/wio/controller/LectureController.java
+++ wonione/src/main/java/cafe24/wio/controller/LectureController.java
@@ -22,6 +22,28 @@ public class LectureController {
 	@Autowired
 	private LectureService lectureService;
 
+	
+	//강의예정코드로 중복개설되었는지 확인
+	@PostMapping(value="/checkLecOpen",produces = "application/json")
+	@ResponseBody
+	public int checkLecOpen(@RequestParam(value="lecOsCode",required = false)String lecOsCode) {
+			int checkResult = lectureService.checkLecOpen(lecOsCode);
+		return checkResult;
+	}
+	
+	
+	//개설된 강의상태 변경하기 ajax
+	@PostMapping(value="/changeLecStatus", produces = "application/json")
+	@ResponseBody
+	public int changeLecStatus(  @RequestParam(value="lecOpCode", required = false)String lecOpCode
+								,@RequestParam(value="selectLecStatusVal", required = false)String changeLecStatus) {
+								
+		int changeResult = lectureService.changeLecStatus(changeLecStatus, lecOpCode);
+		
+		return changeResult;
+	}
+	
+	
 	//강의예정리스트 1개만 조회 ajax
 	@PostMapping(value="/getLecOsListOnly",produces = "application/json")
 	@ResponseBody
@@ -109,10 +131,11 @@ public class LectureController {
 	
 	//강의예정리스트 조회
 	@GetMapping("/lecOsList")
-	public String lecOsList(Model model) {
+	public String lecOsList(Model model
+							,HttpSession session) {
 		
 		/* List<Object> lecApplyCount = lectureService.getApplyCount(); */
-		
+		String lecOpCode = lectureService.getLecOpenCode();
 		List<Map<String,Object>> lectureOsList
 						= lectureService.getLectureOsList();
 		
@@ -120,6 +143,8 @@ public class LectureController {
 		model.addAttribute("mainTitle", "강의 예정 리스트");
 		/* model.addAttribute("lecApplyCount", lecApplyCount); */
 		model.addAttribute("lectureOsList", lectureOsList);
+		model.addAttribute("lecOpCode", lecOpCode);
+		model.addAttribute("sessionId", session.getAttribute("SID").toString());
 		
 		return "lecture/lecOsList";
 	}
